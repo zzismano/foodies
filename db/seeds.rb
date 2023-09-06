e6dc924011c3735053fbd5a28282f6b72b91b3c7
Caio Centeio <cfcenteio@gmail.com>
2023-09-06 17:43:31 -0300

    third push of the day, fixed and improved

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ Recipe.destroy_all
 Meal.destroy_all
 
 p "Seeding users..."
-user1 = User.create(username: "Remy", first_name: "Remy", last_name: "Oswalt", city: "Paris", country: "France", age: 10, email: "rat_chef@foodies.go", password: "123456")
-user2 = User.create(username: "Gusteau", first_name: "Gusteau", last_name: "Garrett", city: "Paris", country: "France", age: 132, email: "master_chef@foodies.go", password: "654321")
-user3 = User.create(username: "Linguini", first_name: "Linguini", last_name: "Romano", city: "Paris", country: "France", age: 22, email: "pasta_chef@foodies.go", password: "123654")
-user4 = User.create(username: "Colette", first_name: "Colette", last_name: "Garofalo", city: "Paris", country: "France", age: 23, email: "victory_chef@foodies.go", password: "654123")
-user5 = User.create(username: "Anton", first_name: "Anton", last_name: "Ego", city: "Paris", country: "France", age: 47, email: "review_chef@foodies.go", password: "321456")
+user1 = User.create!(username: "Remy", first_name: "Remy", last_name: "Oswalt", city: "Paris", country: "France", age: 10, email: "rat_chef@foodies.go", password: "123456")
+user2 = User.create!(username: "Gusteau", first_name: "Gusteau", last_name: "Garrett", city: "Paris", country: "France", age: 132, email: "master_chef@foodies.go", password: "654321")
+user3 = User.create!(username: "Linguini", first_name: "Linguini", last_name: "Romano", city: "Paris", country: "France", age: 22, email: "pasta_chef@foodies.go", password: "123654")
+user4 = User.create!(username: "Colette", first_name: "Colette", last_name: "Garofalo", city: "Paris", country: "France", age: 23, email: "victory_chef@foodies.go", password: "654123")
+user5 = User.create!(username: "Anton", first_name: "Anton", last_name: "Ego", city: "Paris", country: "France", age: 47, email: "review_chef@foodies.go", password: "321456")
 
 file1 = URI.open("https://ih1.redbubble.net/image.999574152.6606/flat,750x1000,075,f.jpg")
 user1.photo.attach(io: file1, filename: "#{user1.username}profile.png", content_type: "image/png")
@@ -34,7 +34,7 @@ file5 = URI.open("https://media.licdn.com/dms/image/C5612AQEtaPzav6ILVg/article-
 user5.photo.attach(io: file5, filename: "profile.png", content_type: "image/png")
 
 p "Seeding Recipes..."
-recipe1 = Recipe.create(
+recipe1 = Recipe.create!(
   title:"Filet Mignon",
   ingredients:"A Filet Mignon steak of any size",
   instructions:"For a medium steak, we cooked our steak at medium-high heat on the stovetop for around 5 minutes, then flipped, added our butter, and cooked for an additional 3-5 minutes. We then transferred our skillet to a 400° oven and cooked for around 5 minutes. Tip: Check the temperature of your meat before transferring to the oven! That way, you can see how far you are from your desired temperature.",
@@ -44,7 +44,7 @@ recipe1 = Recipe.create(
 file1 = URI.open("https://ih1.redbubble.net/image.999574152.6606/flat,750x1000,075,f.jpg")
 recipe1.photo.attach(io: file1, filename: "recipe_post.png", content_type: "image/png")
 
-recipe2 = Recipe.create(
+recipe2 = Recipe.create!(
   title:"Romeo and Juliet Cheesecake",
   ingredients:"1 ¼ cup maria cookie crumbs, ¼ cup butter melted, 750g cream cheese, ½ cup sugar, 2 tsp vanilla extract, 3 eggs, 300-400g guava paste",
   instructions:"Pre-heat oven to 325°F (165°C). Line the bottom (circle) and sides (long strip, optional) of a 9-inch (23 cm) springform pan with parchment paper.
@@ -61,7 +61,7 @@ recipe2 = Recipe.create(
 file2 = URI.open("https://www.daninoce.com.br/wp-content/uploads/2017/09/cheesecake-romeu-e-julieta-1.png")
 recipe2.photo.attach(io: file2, filename: "recipe_post.png", content_type: "image/png")
 
-recipe3 = Recipe.create(
+recipe3 = Recipe.create!(
   title:"Ratatouille",
   ingredients:"2 eggplants, 6 roma tomatoes, 2 yellow squashes, 2 zucchinis",
   instructions:"The different vegetables should be cooked separately, then combined and cooked slowly together until they attain a smooth, creamy consistency so that each vegetable will taste truly of itself.",
@@ -75,8 +75,8 @@ recipe3.photo.attach(io: file3, filename: "recipe_post.png", content_type: "imag
 
 p "Seeding Meals..."
 
-meal1 = Meal.create(title: "Ratatouille", restaurant:"Gusteau's", caption: "Check it out!", lat: "48.87085038918666,", long: "2.343018048001365", user: user1)
-meal2 = Meal.create(title: "Leak and Potato Soup", restaurant:"Gusteau's", caption: "And also this!", lat: "48.87085038918666,", long: "2.343018048001365", user: user1)
+meal1 = Meal.create!(title: "Ratatouille", restaurant:"Gusteau's", caption: "Check it out!", lat: "48.87085038918666", long: "2.343018048001365", user: user1)
+meal2 = Meal.create!(title: "Leak and Potato Soup", restaurant:"Gusteau's", caption: "And also this!", lat: "48.87085038918666,", long: "2.343018048001365", user: user1)
 
 file1 = URI.open("https://ih1.redbubble.net/image.999574152.6606/flat,750x1000,075,f.jpg")
 meal1.photo.attach(io: file1, filename: "post.png", content_type: "image/png")
