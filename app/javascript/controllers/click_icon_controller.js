b121eb5d863850d52d1eff769d4f3c31b41a2a5f
Alicja Surzyn <alicja.surzyn@gmail.com>
2023-09-11 09:47:12 -0300

    "Updates to meal card and profile page"

--- app/javascript/controllers/click_icon_controller.js
+++ app/javascript/controllers/click_icon_controller.js
@@ -2,35 +2,41 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="click-icon"
 export default class extends Controller {
-  static targets = ["heart", "comment", "share", "comment-icon"]
+  static targets = ["heart", "comment", "share", "comment-icon", "show-comments"]
   connect() {
     // console.log("hello from the controller");
-  }
-
-  createComment() {
-    const comment = this.commentTarget.value;
-    const commentId = this.data.get("postId");
-    fetch('/comments/new', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-      },
-      body: JSON.stringify({ post_id: postId, content: content })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Comment created:', data);})
   }
 
   showComment() {
     this.commentTarget.classList.toggle("d-none")
   }
+
+  toggle() {
+    this.showCommentsTarget.classList.toggle("hidden")
+  }
+
+  toggleHeart() {
+    this.heartTarget.classList.toggle("red-heart")
+    // this.iconTarget.classList.toggle("fa-heart-solid")
 }
 
-//   toggleHeart() {
+  async share(e) {
+    e.preventDefault();
 
-// }
+    const shareData = {
+      url: this.data.get("urlValue"),
+    };
+    try {
+      await navigator.share(shareData);
+      this.resultTarget.textContent = "MDN shared successfully";
+    } catch (err) {
+      this.resultTarget.textContent = `Error: ${err}`;
+    }
+  }
+}
+
+
+
 
 //   handleShare() {
 
