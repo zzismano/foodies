a814017310bd95d29401c7cfb86692d8ef59b971
Caio Centeio <cfcenteio@gmail.com>
2023-09-13 18:03:52 -0300

    with new spacers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,22 +1,46 @@
 import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl' // Don't forget this!
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  connect() {
+  static values = {
+    apiKey: String,
+    markers: Array
   }
-  addMarkersToMap() {
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
 
       // Create a HTML element for your custom marker
       const customMarker = document.createElement("div")
       customMarker.innerHTML = marker.marker_html
 
       // Pass the element as an argument to the new marker
-      new mapboxgl.Marker(customMarker)
-        .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // Add this
         .addTo(this.map)
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
