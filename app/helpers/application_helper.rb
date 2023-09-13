8ed3c3d0cf371903272e449f15dce1fe4c27c6ce
Pedro Bolliger <pedrobolliger@gmail.com>
2023-09-13 17:39:46 -0300

    default-pic part I

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_profile_photo(user)
+    if user.photo.attached?
+      cl_image_tag @user.photo.key, class: "circular-image"
+    else
+      render "shared/placeholder_pic"
+    end
+  end
 end
