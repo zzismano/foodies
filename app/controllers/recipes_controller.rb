72610f0ddb413713bad16f19a04fb156a7d2001e
Pedro Bolliger <pedrobolliger@gmail.com>
2023-09-15 11:50:36 -0300

    recovery

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -53,6 +53,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :ingredients, :instructions, :caption)
+    params.require(:recipe).permit(:title, :ingredients, :instructions, :caption, :photo)
   end
 end
